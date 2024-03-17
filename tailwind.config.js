04ba9bbb0e3e0766a6463711edcb35fc5594201d
Xasan0 <xasanceek0733@gmail.com>
2024-03-17 18:00:49 +0500

    added new page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./pages/services.html"],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
